fa966636d742c6115c178d8570b23c5f50ffafc8
pranu79 <93911388+pranu79@users.noreply.github.com>
2025-11-06 22:21:03 +0530

    Update HomeController.java

--- src/main/java/com/demo/controller/HomeController.java
+++ src/main/java/com/demo/controller/HomeController.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/demo/")
 public class HomeController {
 	
-	@GetMapping()
+	@GetMapping("/demo")
 	public String home(){
 		return "home.html";
 	}
